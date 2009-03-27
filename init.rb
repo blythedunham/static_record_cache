df56f7c9745794c705283176f6d2b74c31464274
Blythe Dunham <blythedunham@gmail.com>
2009-03-26 19:26:36 -0600

    Fix initalization

--- init.rb
+++ init.rb
@@ -1,5 +1,5 @@
 require 'ostruct'
 begin ; require 'active_record' ; rescue LoadError; require 'rubygems'; require 'active_record'; end
 
-require 'lib/static_active_record_context'
-require 'lib/acts_as_static_record'
+require 'static_active_record_context'
+require 'acts_as_static_record'
